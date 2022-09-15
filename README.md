e85ead8d0d7981e8b0b96133deed858431ab3db0
una <unagamix@gmail.com>
2022-09-16 01:15:19 +0900

    initial

---
+++ README.md
@@ -0,0 +1,55 @@
+# google-app-script-api-example
+
+## Install
+
+1. Download clasp
+
+   ```sh
+   % npm install -g @google/clasp
+   ```
+
+## Setup
+
+1. Install modules
+
+   ```sh
+   % yarn install
+   ```
+
+1. Login
+
+   ```sh
+   % clasp login
+   ```
+
+1. Confirm your script id
+
+   ```sh
+   % clasp list
+   ```
+
+1. Copy the `.env`
+
+   ```sh
+   % cp .env.example .env
+   ```
+
+1. Set `YOUR_SCRIPT_ID` in `.env`
+
+   ```sh
+   YOUR_SCRIPT_ID= ...
+   ```
+
+1. Do update for `.clasp.json`
+
+   ```sh
+   % yarn setup
+   ```
+
+## Deploy
+
+1. Force writes all local files to `script.google.com`
+
+   ```sh
+   % yarn deploy
+   ```
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+const doGet = (e: GoogleAppsScript.Events.DoGet) => {
+  const body = {
+    message: `GET`,
+  };
+  const response = ContentService.createTextOutput();
+  response.setMimeType(ContentService.MimeType.JSON);
+  response.setContent(JSON.stringify(body));
+  return response;
+};
+
+const doPost = (e: GoogleAppsScript.Events.DoPost) => {
+  let body = {};
+  try {
+    const { type, contents } = e.postData || {
+      type: ``,
+      contents: null,
+    };
+    body = {
+      contentType: type,
+      request: analyzeJSON(contents),
+    };
+  } catch {
+    body = {
+      error: `Internal Server Error`,
+    };
+  }
+  const response = ContentService.createTextOutput();
+  response.setMimeType(ContentService.MimeType.JSON);
+  response.setContent(JSON.stringify(body));
+  return response;
+};
+
+const analyzeJSON = (rawJSON: string): object | null => {
+  try {
+    return JSON.parse(rawJSON);
+  } catch {
+    return null;
+  }
+};
